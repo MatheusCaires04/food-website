d0e73838d129bc35664524063807173b13949447
MatheusCaires04 <matheus-bini@hotmail.com>
2022-11-10 11:08:24 -0300

    Projeto em andamento

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
 
 import Button from "./Button";
 import { HeroSlide } from "./ListHero";
 
 import "swiper/css";
+import "swiper/css/navigation";
 
 const Hero = () => {
   return (
     <div className="pt-20 w-full h-screen ">
-      <Swiper spaceBetween={0} slidesPerView={1} className="h-full">
+      <Swiper
+        spaceBetween={0}
+        slidesPerView={1}
+        modules={[Navigation]}
+        navigation={true}
+        className="h-full"
+      >
         {HeroSlide.map((item, index) => {
           return (
             <SwiperSlide key={index}>
@@ -20,11 +28,11 @@ const Hero = () => {
                   className="absolute top-0 left-0 w-full h-full -z-20 object-cover"
                 />
                 <div className="absolute top-0 left-0 w-full h-full bg-black/70 -z-10" />
-                <div className="px-4 md:px-0 flex flex-col items-center w-full md:w-[33rem]  text-center">
+                <div className="px-4 md:px-0 flex flex-col items-center w-[90%] sm:w-[25rem] md:w-[33rem]  text-center">
                   <span className="text-light-white text-2xl font-semibold">
                     {item.subtitle}
                   </span>
-                  <h1 className="text-white text-6xl md:text-8xl font-semibold mt-2 mb-6">
+                  <h1 className="text-white text-6xl sm:text-7xl md:text-8xl font-semibold mt-2 mb-6">
                     {item.title}
                   </h1>
                   <Button text="Get Started" />
